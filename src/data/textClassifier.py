437d77b9029e947e021164bfb0321133667ae83a
lexwinko <lexwinko@gmail.com>
2020-05-13 22:38:44 +0200

    Fixed reddit classification; added word unigrams

--- src/data/textClassifier.py
+++ src/data/textClassifier.py
@@ -8,6 +8,8 @@ import csv
 #from ktrain import text as txt
 
 
+from tpot import TPOTClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn import preprocessing
 from sklearn.ensemble import GradientBoostingClassifier
@@ -23,7 +25,33 @@ from sklearn.metrics import accuracy_score
 
 
 def classifyData(file, method):
-	data = pd.read_csv(file, header=0, sep=',')
+	data = pd.read_csv(file, header=None, sep=',', skiprows=1)
+	data.columns = ['correctedSentence', 'originalSentence', 'elongated','caps','sentenceLength','sentenceWordLength','spellDelta', 'charTrigrams','wordBigrams','wordUnigrams', 'hashtag', 'url', 'atUser','#','@','E',',','~','U','A','D','!','N','P','O','R','&','L','Z','^','V','$','G','T','X','S','Y','M','langFam', 'lang', 'user',
+					'charTrigrams_similarity_French', 
+					'wordBigrams_similarity_French',
+					'wordUnigrams_similarity_French',
+					'charTrigrams_similarity_German',
+					'wordBigrams_similarity_German',
+					'wordUnigrams_similarity_German',
+					'charTrigrams_similarity_Greek',
+					'wordBigrams_similarity_Greek',
+					'wordUnigrams_similarity_Greek',
+					'charTrigrams_similarity_Indian',
+					'wordBigrams_similarity_Indian',
+					'wordUnigrams_similarity_Indian',
+					'charTrigrams_similarity_Russian',
+					'wordBigrams_similarity_Russian',
+					'wordUnigrams_similarity_Russian',
+					'charTrigrams_similarity_Japanese',
+					'wordBigrams_similarity_Japanese',
+					'wordUnigrams_similarity_Japanese',
+					'charTrigrams_similarity_Turkish',
+					'wordBigrams_similarity_Turkish',
+					'wordUnigrams_similarity_Turkish',
+					'charTrigrams_similarity_English',
+					'wordBigrams_similarity_English',
+					'wordUnigrams_similarity_English']
+	data = data[data.correctedSentence.str.contains('correctedSentence') == False]
 	data.sample(frac=1., random_state=int(time.time()))
 	classes = pd.get_dummies(pd.Series(list(data['lang'])))
 	lang = ['French', 'German', 'Greek', 'English', 'Indian', 'Japanese', 'Russian', 'Turkish']
@@ -59,86 +87,102 @@ def classifyData(file, method):
 					'S',
 					'Y',
 					'M',
-					'charNGrams_similarity_French',
-					'wordNGrams_similarity_French',
-					'charNGrams_similarity_German',
-					'wordNGrams_similarity_German',
-					'charNGrams_similarity_Greek',
-					'wordNGrams_similarity_Greek',
-					'charNGrams_similarity_Indian',
-					'wordNGrams_similarity_Indian',
-					'charNGrams_similarity_Russian',
-					'wordNGrams_similarity_Russian',
-					'charNGrams_similarity_Japanese',
-					'wordNGrams_similarity_Japanese',
-					'charNGrams_similarity_Turkish',
-					'wordNGrams_similarity_Turkish',
-					'charNGrams_similarity_English',
-					'wordNGrams_similarity_English']
+					'charTrigrams_similarity_French',
+					'wordBigrams_similarity_French',
+					'wordUnigrams_similarity_French',
+					'charTrigrams_similarity_German',
+					'wordBigrams_similarity_German',
+					'wordUnigrams_similarity_German',
+					'charTrigrams_similarity_Greek',
+					'wordBigrams_similarity_Greek',
+					'wordUnigrams_similarity_Greek',
+					'charTrigrams_similarity_Indian',
+					'wordBigrams_similarity_Indian',
+					'wordUnigrams_similarity_Indian',
+					'charTrigrams_similarity_Russian',
+					'wordBigrams_similarity_Russian',
+					'wordUnigrams_similarity_Russian',
+					'charTrigrams_similarity_Japanese',
+					'wordBigrams_similarity_Japanese',
+					'wordUnigrams_similarity_Japanese',
+					'charTrigrams_similarity_Turkish',
+					'wordBigrams_similarity_Turkish',
+					'wordUnigrams_similarity_Turkish',
+					'charTrigrams_similarity_English',
+					'wordBigrams_similarity_English',
+					'wordUnigrams_similarity_English']
 
 	le = preprocessing.LabelEncoder()
 	le.fit(lang)
 	print(le.classes_)
 
-	featuredata = data[features].to_numpy()
+	featuredata = data[features].to_numpy(dtype='float64')
 	labeldata = le.transform(data['lang'])
-
-	kf = KFold(n_splits=10)
+	
 
 	model_results = {'GDC':[], 'SGD':[], 'ICA':[], 'XGB':[]}
+	tpot = TPOTClassifier(generations=5, population_size=50, verbosity=2)
 
+	kf = KFold(n_splits=5)
+	split_nr = 1
 	for train, test in kf.split(featuredata):
+		print(len(train),len(test))
 
-		# Average CV score on the training set was: 0.6931966449207828
-		exported_pipeline = make_pipeline(
-		    StackingEstimator(estimator=LinearSVC(C=10.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=0.001)),
-		    Normalizer(norm="max"),
-		    GradientBoostingClassifier(learning_rate=0.1, max_depth=9, max_features=0.2, min_samples_leaf=18, min_samples_split=20, n_estimators=100, subsample=0.6500000000000001)
-		)
-		# Fix random state for all the steps in exported pipeline
-		#set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+		tpot.fit(featuredata[train], labeldata[train])
+		print(tpot.score(featuredata[test], labeldata[test]))
+		tpot.export('tpot_ksplit_'+str(split_nr)+'.py')
+		split_nr+=1
 
-		exported_pipeline.fit(featuredata[train], labeldata[train])
-		results = exported_pipeline.predict(featuredata[test])
-		accuracy = accuracy_score(labeldata[test], results)
-		model_results['GDC'].append([accuracy,train,test])
+		# # Average CV score on the training set was: 0.6931966449207828
+		# exported_pipeline = make_pipeline(
+		#     StackingEstimator(estimator=LinearSVC(C=10.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=0.001)),
+		#     Normalizer(norm="max"),
+		#     GradientBoostingClassifier(learning_rate=0.1, max_depth=9, max_features=0.2, min_samples_leaf=18, min_samples_split=20, n_estimators=100, subsample=0.6500000000000001)
+		# )
+		# # Fix random state for all the steps in exported pipeline
+		# #set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
-		# Average CV score on the training set was: 0.748572421636628
-		exported_pipeline = make_pipeline(
-		    StackingEstimator(estimator=SGDClassifier(alpha=0.001, eta0=1.0, fit_intercept=False, l1_ratio=1.0, learning_rate="invscaling", loss="squared_hinge", penalty="elasticnet", power_t=0.0)),
-		    LinearSVC(C=15.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
-		)
-		# Fix random state for all the steps in exported pipeline
-		#set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+		# exported_pipeline.fit(featuredata[train], labeldata[train])
+		# results = exported_pipeline.predict(featuredata[test])
+		# accuracy = accuracy_score(labeldata[test], results)
+		# model_results['GDC'].append([accuracy,train,test])
 
-		exported_pipeline.fit(featuredata[train], labeldata[train])
-		results = exported_pipeline.predict(featuredata[test])
-		accuracy = accuracy_score(labeldata[test], results)
-		model_results['SGD'].append([accuracy,train,test])
+		# # Average CV score on the training set was: 0.748572421636628
+		# exported_pipeline = make_pipeline(
+		#     StackingEstimator(estimator=SGDClassifier(alpha=0.001, eta0=1.0, fit_intercept=False, l1_ratio=1.0, learning_rate="invscaling", loss="squared_hinge", penalty="elasticnet", power_t=0.0)),
+		#     LinearSVC(C=15.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
+		# )
+		# # Fix random state for all the steps in exported pipeline
+		# #set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
-		# Average CV score on the training set was: 0.7373165618448637
-		exported_pipeline = make_pipeline(
-		    FastICA(tol=0.9500000000000001),
-		    LinearSVC(C=15.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
-		)
-		# Fix random state for all the steps in exported pipeline
-		#set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+		# exported_pipeline.fit(featuredata[train], labeldata[train])
+		# results = exported_pipeline.predict(featuredata[test])
+		# accuracy = accuracy_score(labeldata[test], results)
+		# model_results['SGD'].append([accuracy,train,test])
 
-		exported_pipeline.fit(featuredata[train], labeldata[train])
-		results = exported_pipeline.predict(featuredata[test])
-		accuracy = accuracy_score(labeldata[test], results)
-		model_results['ICA'].append([accuracy,train,test])
+		# # Average CV score on the training set was: 0.7373165618448637
+		# exported_pipeline = make_pipeline(
+		#     FastICA(tol=0.9500000000000001),
+		#     LinearSVC(C=15.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
+		# )
+		# # Fix random state for all the steps in exported pipeline
+		# #set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
-		# Average CV score on the training set was: 0.7121593291404611
-		exported_pipeline = XGBClassifier(learning_rate=0.1, max_depth=9, min_child_weight=14, n_estimators=100, nthread=1, subsample=0.8500000000000001)
-		# Fix random state in exported estimator
-		#if hasattr(exported_pipeline, 'random_state'):
-		#    setattr(exported_pipeline, 'random_state', 42)
+		# exported_pipeline.fit(featuredata[train], labeldata[train])
+		# results = exported_pipeline.predict(featuredata[test])
+		# accuracy = accuracy_score(labeldata[test], results)
+		# model_results['ICA'].append([accuracy,train,test])
 
-		exported_pipeline.fit(featuredata[train], labeldata[train])
-		results = exported_pipeline.predict(featuredata[test])
-		accuracy = accuracy_score(labeldata[test], results)
-		model_results['XGB'].append([accuracy,train,test])
+		# # Average CV score on the training set was: 0.7121593291404611
+		# exported_pipeline = XGBClassifier(learning_rate=0.1, max_depth=9, min_child_weight=14, n_estimators=100, nthread=1, subsample=0.8500000000000001)
+		# # Fix random state in exported estimator
+		# #if hasattr(exported_pipeline, 'random_state'):
+		# #    setattr(exported_pipeline, 'random_state', 42)
+
+		# exported_pipeline.fit(featuredata[train], labeldata[train])
+		# results = exported_pipeline.predict(featuredata[test])
+		# accuracy = accuracy_score(labeldata[test], results)
+		# model_results['XGB'].append([accuracy,train,test])
 
 	#print(model_results['GDC'][0],len(model_results['GDC']))
 	#print(model_results['GDC'][0][0])
