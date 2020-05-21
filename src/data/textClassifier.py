ba1fd4f338feba5b7262a58b05e11e369af15dbb
lexwinko <lexwinko@gmail.com>
2020-05-21 17:37:25 +0200

    Fixed tpot model and kfold usage

--- src/data/textClassifier.py
+++ src/data/textClassifier.py
@@ -12,16 +12,13 @@ from tpot import TPOTClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn import preprocessing
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.linear_model import SGDClassifier
-from sklearn.decomposition import FastICA
 from xgboost import XGBClassifier
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Normalizer
-from sklearn.svm import LinearSVC
 from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier
+from sklearn.feature_selection import RFE
+from sklearn.pipeline import make_pipeline, make_union
 
 
 def classifyData(file, method):
@@ -118,81 +115,53 @@ def classifyData(file, method):
 
 	featuredata = data[features].to_numpy(dtype='float64')
 	labeldata = le.transform(data['lang'])
+
+	X_train, X_test, y_train, y_test = train_test_split(featuredata, labeldata, test_size=0.3)
 	
 
-	model_results = {'GDC':[], 'SGD':[], 'ICA':[], 'XGB':[]}
-	tpot = TPOTClassifier(generations=5, population_size=50, verbosity=2)
+	model_results = {'Training':[], 'KFold':[]}
+	#tpot = TPOTClassifier(generations=5, population_size=50, verbosity=2)
 
-	kf = KFold(n_splits=5)
-	split_nr = 1
-	for train, test in kf.split(featuredata):
-		print(len(train),len(test))
+	
 
-		tpot.fit(featuredata[train], labeldata[train])
-		print(tpot.score(featuredata[test], labeldata[test]))
-		tpot.export('tpot_ksplit_'+str(split_nr)+'.py')
-		split_nr+=1
+	
 
-		# # Average CV score on the training set was: 0.6931966449207828
-		# exported_pipeline = make_pipeline(
-		#     StackingEstimator(estimator=LinearSVC(C=10.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=0.001)),
-		#     Normalizer(norm="max"),
-		#     GradientBoostingClassifier(learning_rate=0.1, max_depth=9, max_features=0.2, min_samples_leaf=18, min_samples_split=20, n_estimators=100, subsample=0.6500000000000001)
-		# )
-		# # Fix random state for all the steps in exported pipeline
-		# #set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
-		# exported_pipeline.fit(featuredata[train], labeldata[train])
-		# results = exported_pipeline.predict(featuredata[test])
-		# accuracy = accuracy_score(labeldata[test], results)
-		# model_results['GDC'].append([accuracy,train,test])
+	#tpot.fit(X_train, y_train)
+	#print(tpot.score(X_test, y_test))
+	#tpot.export('tpot_model.py')
 
-		# # Average CV score on the training set was: 0.748572421636628
-		# exported_pipeline = make_pipeline(
-		#     StackingEstimator(estimator=SGDClassifier(alpha=0.001, eta0=1.0, fit_intercept=False, l1_ratio=1.0, learning_rate="invscaling", loss="squared_hinge", penalty="elasticnet", power_t=0.0)),
-		#     LinearSVC(C=15.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
-		# )
-		# # Fix random state for all the steps in exported pipeline
-		# #set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+	exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.1, max_depth=3, max_features=0.7500000000000001, min_samples_leaf=17, min_samples_split=17, n_estimators=100, subsample=0.5)),
+    RFE(estimator=ExtraTreesClassifier(criterion="gini", max_features=0.5, n_estimators=100), step=0.9000000000000001),
+    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.25, min_samples_leaf=14, min_samples_split=2, n_estimators=100)
+	)
 
-		# exported_pipeline.fit(featuredata[train], labeldata[train])
-		# results = exported_pipeline.predict(featuredata[test])
-		# accuracy = accuracy_score(labeldata[test], results)
-		# model_results['SGD'].append([accuracy,train,test])
+	exported_pipeline.fit(X_train, y_train)
+	model_results['Training'] = {'prediction':exported_pipeline.predict(X_test), 'actual':y_test}
 
-		# # Average CV score on the training set was: 0.7373165618448637
-		# exported_pipeline = make_pipeline(
-		#     FastICA(tol=0.9500000000000001),
-		#     LinearSVC(C=15.0,max_iter=120000, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)
-		# )
-		# # Fix random state for all the steps in exported pipeline
-		# #set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
-		# exported_pipeline.fit(featuredata[train], labeldata[train])
-		# results = exported_pipeline.predict(featuredata[test])
-		# accuracy = accuracy_score(labeldata[test], results)
-		# model_results['ICA'].append([accuracy,train,test])
-
-		# # Average CV score on the training set was: 0.7121593291404611
-		# exported_pipeline = XGBClassifier(learning_rate=0.1, max_depth=9, min_child_weight=14, n_estimators=100, nthread=1, subsample=0.8500000000000001)
-		# # Fix random state in exported estimator
-		# #if hasattr(exported_pipeline, 'random_state'):
-		# #    setattr(exported_pipeline, 'random_state', 42)
-
-		# exported_pipeline.fit(featuredata[train], labeldata[train])
-		# results = exported_pipeline.predict(featuredata[test])
-		# accuracy = accuracy_score(labeldata[test], results)
-		# model_results['XGB'].append([accuracy,train,test])
+	kfold_splits = 10
+	kf = KFold(n_splits=kfold_splits)
+	for train, test in kf.split(X_train):
+		exported_pipeline.fit(X_train[train], y_train[train])
+		model_results['KFold'].append({'prediction':exported_pipeline.predict(X_train[test]), 'actual':y_train[test]})
 
 	#print(model_results['GDC'][0],len(model_results['GDC']))
 	#print(model_results['GDC'][0][0])
 	#average =[sum(x[0])/len(model_results['GDC']) for x in model_results['GDC']]
 	#print(average)
-	for model in model_results:
-		print(model)
-		average = 0
-		average = [(average+value[0])/len(model_results[model]) for value in model_results[model]]
-		print(sum(average))
+	#for model in model_results:
+	#	print(model)
+	#	average = 0
+	#	average = [(average+value[0])/len(model_results[model]) for value in model_results[model]]
+	#	print(sum(average))
+
+	print('Training Accuracy: ',accuracy_score(model_results['Training']['actual'],model_results['Training']['prediction']))
+
+	print('KFold Average Accuracy: '+str(sum([accuracy_score(model_results['KFold'][x]['actual'],model_results['KFold'][x]['prediction']) for x in range(0,kfold_splits)])/kfold_splits))
+	for entry in model_results['KFold']:
+		print(accuracy_score(entry['actual'], entry['prediction']))
 			
 			
 
